03c28ef3e39321a2fd05c2c53789ec56a96abb60
Joon <83147743+joonkimresearch@users.noreply.github.com>
2022-11-30 10:42:45 -0500

    Update twitter_config.py

--- twitter_config.py
+++ twitter_config.py
@@ -1,5 +1,5 @@
 # Set twitter credentials
-consumer_key = '2k15CtNmENq4XVmigx20fVQYZ'
-consumer_secret = 'lYiSPwxafcykI29X9RZRnP6tSaPWwzxm6OLoMjelEirggZZhNY'
-access_token = '1385071865937358855-2LtIan286bUbnwOamt23U0OEM1Zy05'
-access_token_secret = '8fVBkLlF1HFiH4DC2xmtWB3uB1fAXaLZ1GAZwfFrKlubs'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
